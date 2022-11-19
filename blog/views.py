ea2a35699373a37e1309d8a425f1f3101d5f0f94
Kanhaiya <balkrishnakanhaiya125@gmail.com>
2022-11-19 14:47:20 +0530

    Removing unwanted code i.e blog app

--- blog/views.py
+++
@@ -1,7 +0,0 @@
-
-
-# Create your views here.
-from django.shortcuts import render
-from django.http import HttpResponse
-def home(request):
-    return HttpResponse('this is blog website')
--- mac/settings.py
+++ mac/settings.py
@@ -42,8 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog',
-    # 'shop'
+    
 ]
 
 MIDDLEWARE = [
